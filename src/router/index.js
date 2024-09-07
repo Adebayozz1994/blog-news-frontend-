2722679c3b5bfdbccdc49c1fa13e433c56fef6f3
Ogunlade Adebayo Peter <144332360+Adebayozz1994@users.noreply.github.com>
2024-09-07 14:06:21 -0100

    new commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,14 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HelloWorld from "../components/HelloWorld.vue";
 import RegisterPage from "../components/RegisterPage.vue"
+import RegisterPageUser from "../components/RegisterPageUser.vue"
 
 const routes = [
     {path: "/", component: HelloWorld},
     {path: "/admin/register", component: RegisterPage},
     {path: "/admin/login", component: RegisterPage},
+    {path: "/register", component: RegisterPageUser},
+    {path: "/login", component: RegisterPageUser},
 
 
 ]
